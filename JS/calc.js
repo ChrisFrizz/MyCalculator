c2c25dacae3e22f2d0d839b3b08fe46085eef74e
christian pellegrini <christianpellegrini16@gmail.com>
2025-10-22 15:37:27 +0200

    scheletro fatto male

---
+++ JS/calc.js
@@ -0,0 +1,21 @@
+let result = 0;
+
+function somma(a,b){
+    return a+b;
+}
+
+function sottrazione(a,b){
+    return a-b;
+}
+
+function multi(a,b){
+    return a*b;
+}
+
+function dividi(a,b){
+    return a/b;
+}
+
+function resto(a,b){
+    return a%b;
+}
