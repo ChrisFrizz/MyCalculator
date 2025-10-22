4e1128fd9896169cbeb657c9db0fb5bf571f0339
christian pellegrini <christianpellegrini16@gmail.com>
2025-10-22 16:47:57 +0200

    Aggiunto evento click + script index html

--- JS/calc.js
+++ JS/calc.js
@@ -1,5 +1,17 @@
 let result = 0;
 
+//array di HTML Button Element
+let allBtn = document.querySelectorAll("button");
+
+allBtn.forEach((btn) => {
+    console.log(btn);
+    btn.addEventListener("click", (event) =>{
+        console.log(event.target);        
+    })    
+});
+
+
+
 function somma(a,b){
     return a+b;
 }
