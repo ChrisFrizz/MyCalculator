5bc05b2bc6f817378880a6659034df6e22a43714
christian pellegrini <christianpellegrini16@gmail.com>
2025-10-27 16:18:23 +0100

    Pulsante da sistemare

--- JS/calc.js
+++ JS/calc.js
@@ -1,6 +1,7 @@
 let val1 = document.getElementById("val1");
 let val2 = document.getElementById("val2");
 let result = document.getElementById("result");
+let memory = document.getElementById("memory");
 
 //array di HTML Button Element
 let allBtn = document.querySelectorAll("button");
@@ -25,8 +26,21 @@ allBtn.forEach((btn) => {
         if (res instanceof Error){
             result.innerText = res.message;
         }
-        else
+        else{
             result.innerText = "Result: " + res;
+
+            let myLog = new MyLog(a,b,operation);
+
+            let btnLoad = document.createElement("button");
+            btnLoad.innerText = "LOAD";
+            btnLoad.addEventListener("click", myLog.fnLoad);
+
+            let logLine = document.createElement("li");
+            logLine.innerText = ("Eseguito: " + myLog.val1 + myLog.operation + myLog.val2);
+
+            memory.appendChild(logLine);
+            logLine.appendChild(logLine);
+        }
     })
 });
 
@@ -53,6 +67,18 @@ function performOperation(a, b, operation){
             return resto(a,b);
         default:
             return Error("Operazione non valida");
+    }
+}
+
+function MyLog(a,b,operation){
+    this.val1 = a;
+    this.val2 = b;
+    this.operation = operation;
+
+    this.fnLoad = () =>{
+        console.log("LOAD", this.val1, this.val2);
+        val1.value = this.val1;
+        val2.value = this.val2;
     }
 }
 
